22d2a6e5812c6b3b5cee9179d08a54cda1090023
SweetVader <xxxnastikxxx@mail.ru>
2019-02-06 15:18:17 +0300

    Add velocity file

--- src/main/java/com/atlassian/tutorial/myPlugin/impl/MyPluginComponentImpl.java
+++ src/main/java/com/atlassian/tutorial/myPlugin/impl/MyPluginComponentImpl.java
@@ -1,19 +1,14 @@
 package com.atlassian.tutorial.myPlugin.impl;
 
-import com.atlassian.jira.issue.tabpanels.GenericMessageAction;
-import com.atlassian.jira.plugin.issuetabpanel.AbstractIssueTabPanel3;
-import com.atlassian.jira.plugin.issuetabpanel.GetActionsRequest;
-import com.atlassian.jira.plugin.issuetabpanel.IssueAction;
-import com.atlassian.jira.plugin.issuetabpanel.ShowPanelRequest;
 import com.atlassian.plugin.spring.scanner.annotation.export.ExportAsService;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.sal.api.ApplicationProperties;
 import com.atlassian.tutorial.myPlugin.api.MyPluginComponent;
-import com.google.common.collect.Lists;
+
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.util.List;
+
 
 @ExportAsService ({MyPluginComponent.class})
 @Named ("myPluginComponent")
